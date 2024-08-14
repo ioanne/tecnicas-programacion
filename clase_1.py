ab189ffc458bef904881ce035cc292f50b3b160d
Juan Ignacio Bonini <juanignaciobonini@hotmail.com>
2024-08-13 21:22:05 -0300

    initial commit

---
+++ clase_1.py
@@ -0,0 +1,77 @@
+variable = "Esto es una variable" # str, string (Cadena de texto)
+
+numero = 10 # int, integer (Número entero)
+flotante = 10.5 # float (Número decimal)
+booleano = True # bool, boolean (Valor lógico)
+booleano2 = False # bool, boolean (Valor lógico)
+binario = b'1010' # bytes (Valor binario)
+
+nulo = None # NoneType (Valor nulo)
+
+suma = 10 + 5
+resta = 10 - 5
+multiplicacion = 10 * 5
+division = 10 / 5
+division_entera = 10 // 5
+divison_residuo = 10 % 5
+
+# AND
+# OR
+# NOT
+
+
+# Reglas de las variables.
+# 1. No pueden empezar con números.
+# 2. No pueden tener espacios.
+# 3. No pueden tener caracteres especiales. !"#$%&/(), etc"
+# 4. No pueden ser palabras reservadas. if, else, for, while, etc.
+# 5. Pueden tener guiones bajos al principio. _variable
+
+algo = True
+_algo = False
+
+# Hardcodear
+# Significa que el valor esta fijo y no cambia.
+
+# Condicional simple
+if algo or _algo:
+    print("Hola mundo")
+    algo = False
+
+# Breakpoint
+# Punto de quiebre / Punto de interrupción
+
+# Depurador
+# Debug
+
+if 10 < 5:
+    print("10 es menor que 5")
+
+
+if 10 < 5:
+    print("10 es menor que 5")
+elif 10 == 10:
+    print("10 es igual a 10")
+elif 10 == 10:
+    print("10 es igual a 10")
+elif 10 == 10:
+    print("10 es igual a 10")
+elif 10 == 10:
+    print("10 es igual a 10")
+else:
+    print("10 no es menor que 5")
+"""
+elif = else if
+"""
+# indentacion
+
+
+# Si 10 es menor a 5 muestro 10 es menor a 5
+# de lo contrario muestro 10 no es menor a 5
+
+
+
+print("Fin del programa")
+
+
+
