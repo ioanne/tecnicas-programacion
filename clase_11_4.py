1360aabf8ff688f8b105f46ab2c953d300e20cee
Juan Ignacio Bonini <juanignaciobonini@hotmail.com>
2024-10-16 00:37:06 -0300

    clase 11

--- clase_11_4.py
+++ clase_11_4.py
@@ -1,3 +1,7 @@
+class B:
+    def __init__(self) -> None:
+        print("Creando la clase B")
+
 class A:
     def __init__(self, a, b):
         print("Inicializando variables")
@@ -8,11 +12,17 @@ class A:
 
     def __new__(self, *args, **kwargs):
         print("Creando objeto")
-        return super().__new__(self)
+        return B()
     
     def __del__(self):
         print("Eliminando objeto")
 
 
 a = A(1,2)
-del a
+print(a)
+
+
+a = A()
+b = B()
+
+a + b
